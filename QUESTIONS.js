c909cdc66395674b3fb14e57dba106ba47d6a59a
Kush Panjabi <kushpanjabi@icloud.com>
2020-03-04 14:42:55 -0800

    all wireframe assets

---
+++ QUESTIONS.js
@@ -0,0 +1,63 @@
+const Questions = {
+    questions: [//1
+      {
+        question: "What is the capital of Canada?",
+        options: [
+          "Vancouver", 
+          "Edmonton", 
+          "Ottawa", 
+          "Toronto"
+        ],
+        answer: "Ottawa",
+        photo: "https://specials-images.forbesimg.com/imageserve/1064713266/960x0.jpg?fit=scale"
+      },
+      //2
+      {
+        question: "What is the capital of Indonesia?",
+        options: [
+          "Denpasar (Bali)",
+          "Medan (Sumatra)", 
+          "Jakarta",
+          "Semarang"
+        ],
+        answer: "Jakarta",
+        photo: "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iqAMzxFRr2a0/v1/1000x-1.jpg"
+      },
+      //3
+      {
+        question: "What is the capital of India?",
+        options: [
+          "Delhi", 
+          "Mumbai", 
+          "Bangalore", 
+          "Chennai"
+        ],
+        answer: "Delhi",
+        photo: "https://static.timesofisrael.com/www/uploads/2018/01/F161227NS118-640x400.jpg"
+      },
+      //4
+      {
+        question: "What is the capital of Italy?",
+        options: [
+          "Milan", 
+          "Rome", 
+          "Florence", 
+          "Bologna"
+        ],
+        answer: "Rome",
+        photo: "https://www.wantedinrome.com/i/featured/storage/uploads/2020/03/why-now-is-a-great-time-to-visit-rome_2.jpg"
+      },
+      //5
+      {
+        question: "What is the capital of New Zealand?", 
+        options: [
+            "Christchurch", 
+            "Auckland",
+            "Queenstown",
+            "Wellington"
+        ],
+        answer: "Wellington",
+        photo: "https://img.theculturetrip.com/768x432/wp-content/uploads/2017/01/wellington_city_at_dusk.jpg"
+      }
+    ]
+  };
